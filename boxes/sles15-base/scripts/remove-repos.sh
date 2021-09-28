980e462bf0cfbdf1fd0895d09be892b759ab0b24
Michael Anderson <manderson@hpe.com>
2021-09-28 11:41:56 -0500

    CASMINST-3180: Remove service repos and ensure they are removed

--- boxes/sles15-base/scripts/remove-repos.sh
+++ boxes/sles15-base/scripts/remove-repos.sh
@@ -9,3 +9,8 @@ zypper -n rr buildonly-cray-sle-module-basesystem-updates
 zypper -n rr buildonly-cray-sle-module-basesystem-updates-debug
 zypper -n rr buildonly-cray-sle-module-public-cloud-updates
 zypper -n clean --all
+
+zypper rs Basesystem_Module_15_SP2_x86_64
+zypper rs Public_Cloud_Module_15_SP2_x86_64
+zypper rs SUSE_Linux_Enterprise_Server_15_SP2_x86_64
+zypper rs Server_Applications_Module_15_SP2_x86_64
