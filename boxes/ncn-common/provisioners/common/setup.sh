a8a5c4388d558a44b94998b6fc1b4d9dac7884f9
rustydb <doomslayer@hpe.com>
2022-04-25 09:25:31 -0500

    MTL-1682 Move kernel provisioner
    The kernel provisioner lived in the wrong directory, which was a minor
    aesthetic issue. The main change here ensures that the kernel is not
    locked before csm-rpms is invoked. The issue was that if a newer kernel
    was defined in csm-rpms that it would fail to install due to the zypper
    lock.
    
    MTL-1682 Remove redundant kernel version
    
    We lock the kernel with csm-rpms, this extra kernel definition is
    entirely unnecessary and confusing to maintaining the kernel.
    
    This also drastically cleans up the provisioners for the ncn-common
    layer so they more clearly define contexts.

--- boxes/ncn-common/provisioners/common/setup.sh
+++ boxes/ncn-common/provisioners/common/setup.sh
@@ -22,7 +22,21 @@
 # ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
 # OTHER DEALINGS IN THE SOFTWARE.
 #
-set -e
+set -exu
+
+
+# Find device and partition of /
+cd /
+df . | tail -n 1 | tr -s " " | cut -d " " -f 1 | sed -E -e 's/^([^0-9]+)([0-9]+)$/\1 \2/' |
+if read DEV_DISK DEV_PARTITION_NR && [ -n "$DEV_PARTITION_NR" ]; then
+  echo "Expanding $DEV_DISK partition $DEV_PARTITION_NR";
+  sgdisk --move-second-header
+  sgdisk --delete=${DEV_PARTITION_NR} "$DEV_DISK"
+  sgdisk --new=${DEV_PARTITION_NR}:0:0 --typecode=0:8e00 ${DEV_DISK}
+  partprobe "$DEV_DISK"
+
+  resize2fs ${DEV_DISK}${DEV_PARTITION_NR}
+fi
 
 echo "Initializing log location(s)"
 mkdir -p /var/log/cray
